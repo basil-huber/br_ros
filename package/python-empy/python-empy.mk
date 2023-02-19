602fee158020af779a983b8afde669d7c74e6c6b
basil-huber <basil.huber@gmail.com>
2023-02-19 09:31:17 +0100

    python-empy: fix SITE

--- package/python-empy/python-empy.mk
+++ package/python-empy/python-empy.mk
@@ -6,7 +6,7 @@
 
 PYTHON_EMPY_VERSION = 3.3.4
 PYTHON_EMPY_SOURCE = empy-$(PYTHON_EMPY_VERSION).tar.gz
-PYTHON_EMPY_SITE = http://www.alcyone.com/software/empy
+PYTHON_EMPY_SITE = https://files.pythonhosted.org/packages/3b/95/88ed47cb7da88569a78b7d6fb9420298df7e99997810c844a924d96d3c08
 PYTHON_EMPY_SETUP_TYPE = setuptools
 PYTHON_EMPY_LICENSE = FIXME: please specify the exact BSD version
 
